50809a40830a504dcfc97de04e027a095fab16ac
tate <tate@macusers-mbp-5>
2019-06-27 21:12:59 +0100

    Added Node

--- classes.py
+++ classes.py
@@ -1,7 +1,19 @@
-class A(object):
-    def __init__(self, name):
+class Node(object):
+    children=[]
+    def add_child(self, name):
+        self.children.append(name)
+    def __init__(self, name, *parents):
         self.name = name
-        
-class B(object):
-    def __init__(self, name):
-        self.name = name
+        self.parents = parents
+        for parent in parents:
+            parent.add_child(self)
+    def get_children(self):
+        children=[]
+        for child in self.children:
+            children.append(child.name)
+        return(children)
+    def get_parents(self):
+        parents=[]
+        for parent in self.parents:
+            parents.append(parent.name)
+        return(parents)
